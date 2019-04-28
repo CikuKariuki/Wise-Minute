cf6ba20f08bd2ff933717ef6c7e4680ae109b50a
Wanjiku <Wanjiku>
2019-04-28 23:17:24 +0300

    created functionality for photo uploading

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import config_options
 from app.main import views,errors
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -12,13 +13,15 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
-
+photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
     app = Flask(__name__,instance_relative_config=True)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
+    configure_uploads(app,photos)
+    
     #Creating the app configurations
     app.config.from_object(config_options[config_name])
 
