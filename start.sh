355b1ce65c4fb636434cd0fb9088f5d67cc4e452
Wanjiku <Wanjiku>
2019-04-30 12:29:58 +0300

    Changed to production mode

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ export SECRET_KEY=hjh635dhru584/hf55
 
 
 export MAIL_USERNAME=sheekokariuki@gmail.com
-export MAIL_PASSWORD=mashida nyingi
+export MAIL_PASSWORD=mashidanyingi
 
 python3.6 manage.py server
